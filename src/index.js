caa5f6ecd84cba75f2ba87d5d99f7759986f9e16
Novik <natalyamihalovna88@gmail.com>
2022-08-10 18:07:07 +0300

    finish task

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 module.exports = function reverse (n) {
-    let result = 0;
-    while (n) {
-        result = result * 10 + n % 10;
-        n = Math.floor(n / 10);
-    }
-    return result;
+    // let result = 0;
+    // while (n) {
+    //     result = result * 10 + n % 10;
+    //     n = Math.floor(n / 10);
+    // }
+    // return result;
+
+    const result = parseFloat(
+        n.toString()
+        .split('')
+        .reverse()
+        .join('')
+    ) * Math.sign(n);
+
+    return Math.abs(result);
 }
